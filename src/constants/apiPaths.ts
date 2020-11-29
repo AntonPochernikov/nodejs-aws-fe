0a4b48d43f13282b412bbdd7e74b0895acf1d055
AntonPochernikov <akpochernikov@gmail.com>
2020-11-29 23:11:16 +0300

    task-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   products: 'https://kchlp8es63.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://kchlp8es63.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://kchlp8es63.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://forwg6br9e.execute-api.eu-west-1.amazonaws.com/dev/import',
 };
 
 export default API_PATHS;
